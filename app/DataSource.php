c837458706d83b84ae71563041c5532fd03603f5
Maksim Pecherskiy <max@maksimize.com>
2012-09-05 11:44:50 -0500

    [#35129795] Adding uuid column and central DataSource function for ids/uuids.

--- app/DataSource.php
+++ app/DataSource.php
@@ -1,10 +1,27 @@
 <?php
 
 class DataSource {
-  public static function getGame($game_id) {
-    $query = "SELECT * FROM `games` WHERE id = $game_id";
+
+  /**
+   * Retrieve game by serial id or uuid.
+   *
+   * @param mixed $id
+   *  UUID or ID of the game.
+   */
+  public static function getGame($id) {
+    $search_id_key = DataSource::uuidIsValid($id) ? 'uuid' : 'id';
+    $query = "SELECT * FROM `games` WHERE $search_id_key = $id";
     $result = mysql_query($query);
     $game = mysql_fetch_assoc($result);
     return $game;
+  }
+
+  /**
+   * Verify the validity of a uuid.
+   * @param string $uuid
+   *  UUID to verify.
+   */
+  public static function uuidIsValid($uuid) {
+    return (boolean) preg_match('/^[A-Fa-f0-9]{8}-[A-Fa-f0-9]{4}-[A-Fa-f0-9]{4}-[A-Fa-f0-9]{4}-[A-Fa-f0-9]{12}$/', $uuid);
   }
 }
