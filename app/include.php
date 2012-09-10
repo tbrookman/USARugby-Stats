7834bae9cd0887f1640de647953212921f0e4590
Chris Christensen <christianchristensen@gmail.com>
2012-09-09 21:26:32 -0500

    [#35128837] Initial bootstrap theme inject and formatting.

--- app/include.php
+++ app/include.php
@@ -4,12 +4,35 @@ include_once './DataSource.php';
 include_once './session.php';
 include_once './user_check.php';
 include_once './db.php';
-echo "<html>";
-echo "<head>";
+?>
+
+<html lang="en">
+<head>
+
+<?php
 echo "<title>USA Rugby National Championship Series</title>";
+?>
+
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta name="description" content="">
+  <meta name="author" content="">
+
+  <!-- Styles -->
+  <link href="/assets/vendor/bootstrap/css/bootstrap.css" rel="stylesheet">
+  <link href="/assets/vendor/bootstrap/css/bootstrap-responsive.css" rel="stylesheet">
+
+<?php
 include_once './display_funcs.php';
 include_once './other_funcs.php';
-include_once './include_js.php';
-echo "</head>";
-echo "<body>";
+?>
+
+  <script src='/assets/vendor/jquery.min.js'></script>
+  <script src='/assets/vendor/bootstrap/js/bootstrap.js'></script>
+  <script src='jquery_funcs.js'></script>
+
+</head>
+<body>
+
+<?php
 include_once './header.php';
--- app/include_js.php
+++
@@ -1,3 +0,0 @@
-<?php
-echo "<script src='jquery.js'></script>";
-echo "<script src='jquery_funcs.js'></script>";
