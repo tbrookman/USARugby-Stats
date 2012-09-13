986d942273aa28287a54aec450214854ef121fe7
Maksim Pecherskiy <max@maksimize.com>
2012-09-13 17:35:09 -0500

    [#35129771] Adding event types to schema.

--- db/schema.sql
+++ db/schema.sql
@@ -43,6 +43,7 @@ CREATE TABLE `event_types` (
   `value` int(2) NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=MyISAM AUTO_INCREMENT=16 DEFAULT CHARSET=latin1;
+INSERT INTO `event_types` VALUES (1,1,'Try',5),(2,2,'Conversion',2),(3,3,'Penalty Kick',3),(4,4,'Drop Goal',3),(5,11,'Tactical Sub Off',0),(6,12,'Tactical Sub On',0),(7,13,'Injury Sub Off',0),(8,14,'Injury Sub On',0),(9,15,'Blood Sub Off',0),(10,16,'Blood Sub On',0),(11,21,'Yellow Card',0),(12,22,'Red Card',0),(13,17,'Front Row Card Sub Off',0),(14,18,'Front Row Card Sub On',0),(15,5,'Penalty Try',5);
 /*!40101 SET character_set_client = @saved_cs_client */;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
