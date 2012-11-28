07b04b5f2867e4c91111abd687605465bf372c7d
Maksim Pecherskiy <max@maksimize.com>
2012-11-28 10:32:29 -0600

    [#40290183] Adding DataSource functionality for getting players game events.

--- lib/Source/DataSource.php
+++ lib/Source/DataSource.php
@@ -442,6 +442,34 @@ class DataSource {
         return $result;
     }
 
+    public function getPlayerGameEvents($player_id, $comp_id = NULL, $game_id = NULL) {
+        $player_search_id = DataSource::uuidIsValid($player_id) ? $this->getSerialIDByUUID('players', $player_id) : $player_id;
+        $game_events = array();
+        if (empty($player_search_id)) {
+            return FALSE;
+        }
+        $query = "SELECT
+                  ge.game_id,
+                  ge.player_id,
+                  ge.type,
+                  ge.minute,
+                  evt.name,
+                  evt.value,
+                  gam.home_id,
+                  gam.away_id,
+                  gam.kickoff
+                  FROM game_events ge
+                  JOIN  games gam ON (ge.game_id = gam.id)
+                  JOIN event_types evt ON (ge.type = evt.event_id)
+                  WHERE ge.player_id=$player_search_id";
+        $result = mysql_query($query);
+        while ($row = mysql_fetch_assoc($result)) {
+          $game_events[] = $row;
+        }
+        return $game_events;
+    }
+
+
     public function addPlayer($player_info) {
         $columns = $this->showColumns('players');
         $values = '';
