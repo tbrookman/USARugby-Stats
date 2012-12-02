9f46c26be66de59923f239a48b700b591648dd73
Chris Christensen <christianchristensen@gmail.com>
2012-12-02 15:43:11 -0600

    Cleanup division display on comp listing.

--- app/comp_teams.php
+++ app/comp_teams.php
@@ -14,11 +14,10 @@ while ($row=mysql_fetch_assoc($result)) {
     while ($row2=mysql_fetch_assoc($result2)) {
 
         echo "<td><a href='team.php?id={$row2['id']}'>{$row2['short']}<a/></td>";
+        $division = $db->getTeam($row['division_id']);
+        echo "<td>{$division['name']}</td>";
 
         if (editCheck(1)) {
-            echo "<td><form style='margin: 0; padding: 0' name='dTeam{$row2['id']}' id='dTeam{$row2['id']}'>";
-            $division = $db->getTeam($row['division_id']);
-            echo "{$division['name']}</td>";
             echo "<td><a href='#' class='dTeam' id='dTeam{$row2['id']}' data-del-team-id='{$row2['id']}'> <i class='icon-trash'></i></a>";
             echo "<input type='hidden' class='dId' name='team_id' id='team_id' value='{$row2['id']}' />";
             echo "<input type='hidden' class='dId' name='comp_id' id='comp_id' value='$comp_id' />";
