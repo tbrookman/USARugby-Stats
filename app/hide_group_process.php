69156babfc241d4fb702756ef9fd23ad28cad699
Tasha Brookman <tbrookman@allplayers.com>
2013-01-11 15:58:05 -0600

    [#41855133] delete files not need

--- app/hide_group_process.php
+++
@@ -1,7 +0,0 @@
-<?php
-include_once './include_mini.php';
-
-$id = $_POST['id'];
-
-$query = "UPDATE `teams` SET hidden=1 WHERE id='$uuid'";
-$result = mysql_query($query);
--- app/show_group_process.php
+++
@@ -1,7 +0,0 @@
-<?php
-include_once './include_mini.php';
-
-$id = $_POST['id'];
-
-$query = "UPDATE `teams` SET hidden=0 WHERE id='$uuid'";
-$result = mysql_query($query);
